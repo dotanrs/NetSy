ba63eefbbf3978c087915b4b3b68d97c2718f9f9
Dotan Reis <dotan.reis@here.com>
2017-01-15 10:40:57 +0200

    documantation to phase example

--- examples/phase_space.py
+++ examples/phase_space.py
@@ -6,45 +6,41 @@ from netsy import display as d
 from netsy.factory import NeuronDict as nd
 import random
 
+# Parameters. Leave like this for a cool spiral effect
 jee = 2.02
 j = 1.2
 thee = - 10 * (jee - j)
 thei = - 10 * j
 
+# Create a network with two sigmoid neurons. The definition of one as inihibitory
+# and one as excitatory will be expressed in the connections
 net = n.Network()
-
 inhibitory = net.create_neuron(name="in", ntype=nd.sigmoid, bias=10, tanh_bias=thei, init=10.00001, log=False)
 excitatory = net.create_neuron(name="ex", ntype=nd.sigmoid, bias=10, tanh_bias=thee, init=10.00001, log=False)
 
-
+# Create connections. note that the excitatory neuron listens to itself
 excitatory.listen_to(inhibitory, -j)
 excitatory.listen_to(excitatory, jee)
-
 inhibitory.listen_to(excitatory, j)
 
-activations = net.run_and_get_activations(steps=100000)#00)
+activations = net.run_and_get_activations(steps=100000)
+
+# Print the results
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 fig = plt.figure()
-ax1 = fig.add_subplot(121)
-
-# plt.show()
-
-ax1.scatter(activations[inhibitory],activations[excitatory],color='blue',s=5,edgecolor='none')
-
-N=1000
-x = np.random.randn(N)
-y = np.random.randn(N)
 
 ## left panel
-# ax1.scatter(x,y,color='blue',s=5,edgecolor='none')
-ax1.set_aspect(1./ax1.get_data_ratio()) # make axes square
+ax1 = fig.add_subplot(121)
+ax1.scatter(activations[inhibitory],activations[excitatory],color='blue',s=5,edgecolor='none')
 
+# make axes square
+ax1.set_aspect(1./ax1.get_data_ratio())
 
+## right panel
 plt.subplot(122)
-
 d.show_all_in_graph([excitatory, inhibitory], activations, params="")
 
 
